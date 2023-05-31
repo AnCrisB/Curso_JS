cfae1c4f9d915eecc12fb5bf1ac9ed009a648387
Cristina <www.cristel@gmail.com>
2023-05-31 15:58:11 -0500

    Matriz ordenada

--- CLASE 5_3005/Matriz.js
+++ CLASE 5_3005/Matriz.js
@@ -1,35 +1,33 @@
 //Crear numeros aleatorios en una matriz mxn, luego alinearlos y luego organizarlos de forma sinusoidal
 
-/* var m = [];
+var m = [];
     m [0] = [Math.ceil(Math.random()*10), Math.ceil(Math.random()*10), Math.ceil(Math.random()*10), Math.ceil(Math.random()*10)];
     m [1] = [Math.ceil(Math.random()*10), Math.ceil(Math.random()*10), Math.ceil(Math.random()*10), Math.ceil(Math.random()*10)];
     m [2] = [Math.ceil(Math.random()*10), Math.ceil(Math.random()*10), Math.ceil(Math.random()*10), Math.ceil(Math.random()*10)];
-    m [3] = [Math.ceil(Math.random()*10), Math.ceil(Math.random()*10), Math.ceil(Math.random()*10), Math.ceil(Math.random()*10)]; */
+    m [3] = [Math.ceil(Math.random()*10), Math.ceil(Math.random()*10), Math.ceil(Math.random()*10), Math.ceil(Math.random()*10)];
+    m [4] = [Math.ceil(Math.random()*10), Math.ceil(Math.random()*10), Math.ceil(Math.random()*10), Math.ceil(Math.random()*10)];
 
-const m = [];
+/* const m = [];
     m [0] = [2, 1, 3, 5];
     m [1] = [6, 9, 7, 8];
     m [2] = [9, 8, 1, 0];
     m [3] = [4, 7, 5, 9];
-    m [4] = [8, 2, 6, 1];
+    m [4] = [8, 2, 6, 1]; */
 
-const numeros = [3, 23, 12];
-numeros.sort();
+let = otro = m.flat();
+console.log(otro);
 
-const num = [3, 23, 12];
-num.sort(function(a,b){return b-a});
+let otro2 = otro.sort((a,b) => a-b);
+console.log(otro2);
 
-const array8 = [50, 4, 10, 2, 70, 20];
-console.log(array8.sort((a,b) => a-b)); 
-console.log(array8)
+let m0 = otro2.slice(0,4)
+let m1 = otro2.slice(4,8)
+m1.reverse()
+let m2 = otro2.slice(8,12)
+let m3 = otro2.slice(12,16)
+m3.reverse()
+let m4 = otro2.slice(16,20)
+console.log(m0, m1, m2, m3, m4);
 
-
-console.log(array8.sort((a,b) => b-a)); 
-console.log(array8)
-
-for (i = 0; i < m.length(); i += 3) {
-    var column = m.slice(i, i, + 3);
-    console.log(column.join('/t'))
-}
-
-console.log (m.flat().sort((a,b) => a-b))
+let general = [m0, m1, m2, m3, m4]
+console.log(general)
